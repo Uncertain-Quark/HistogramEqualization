3e6123889bb546f489860997cb38e7f058e71fb1
aditya <aditya16.k@gmail.com>
2021-12-30 10:19:02 +0530

    modified main

--- main.py
+++ main.py
@@ -2,12 +2,36 @@
 import glob, os, sys, torch, numpy
 from makeHistogram import HistEq
 
-sourceWavFolder = sys.argv[1]
-targetWavFolder = sys.argv[2]
-sourceInferenceWavFolder = sys.argv[3]
-targetInferenceWavFolder = sys.argv[4]
+sampleRate = 22050
 
-waveglowCheckpoint = '/'
+def convertRate(folder, destFolder, sampleRate):
+    files = glob.glob(folder + '/*.wav')
+    for f in files : 
+        destfile = destFolder + '/' + f.split('/')[-1]
+        os.system('ffmpeg -i {} -ac 1 -ar {} {}'.format(f, sampleRate, destfile))
+
+sourceFolder = sys.argv[1]
+targetFolder = sys.argv[2]
+sourceInfFolder = sys.argv[3]
+targetInfFolder = sys.argv[4]
+
+sourceWavFolder = 'data/Source'
+targetWavFolder = 'data/Target'
+sourceInferenceWavFolder = 'data/SourceInf'
+targetInferenceWavFolder = 'data/TargetInf'
+
+os.system('mkdir -p data')
+os.system('mkdir -p {}'.format(sourceWavFolder))
+os.system('mkdir -p {}'.format(targetWavFolder))
+os.system('mkdir -p {}'.format(sourceInferenceWavFolder))
+os.system('mkdir -p {}'.format(targetInferenceWavFolder))
+
+convertRate(sourceFolder, sourceWavFolder, sampleRate)
+convertRate(targetFolder, targetWavFolder, sampleRate)
+convertRate(sourceInfFolder, sourceInferenceWavFolder, sampleRate)
+#convertRate(targetInfFolder, targetInferenceWavFolder, sampleRate)
+
+waveglowCheckpoint = '/tts/anusha/journal_phrase_TTS/waveglow/Hindi/male/mono/waveglow_v1_5hrs_using_pretrained_ljspeech_model/checkpoints/waveglow_15000'
 
 sourcePtFolder = './Source'
 targetPtFolder = './Target'
@@ -42,14 +66,15 @@ print(targetMelSpec.shape)
 hist_eq_class = HistEq(80,80)
 hist_eq_class.train(sourceMelSpec.T,targetMelSpec.T)
 
+print(sourceInferencePtFiles)
 for file in sourceInferencePtFiles :
     melInf = torch.load(file).numpy()
     targetMelInf = hist_eq_class.inference(melInf.T)
     targetMelInfPath = targetInferencePtFolder + '/' + file.rstrip('/').split('/')[-1]
-    torch.save(targetMelInf.T, targetMelInfPath)
+    torch.save(torch.from_numpy(targetMelInf.T), targetMelInfPath)
 
 targetInferencePtFiles = glob.glob(targetInferencePtFolder + '/*.pt')
-os.system('ls {}/*.pt > mel_files.txt'.format(targetInferencePtFiles))
-os.system('python3 inference.py -f mel_files.txt -w {} -o {} --is_fp16 -s 0.6'.format(waveglowCheckpoint, targetInferenceWavFolder))
+os.system('ls {}/*.pt > mel_files.txt'.format(targetInferencePtFolder))
+os.system('python3 waveglow/inference.py -f mel_files.txt -w {} -o {} --is_fp16 -s 0.6'.format(waveglowCheckpoint, targetInferenceWavFolder))
 
 #hist_eq_class.inference(sourceMelSpec.T)
